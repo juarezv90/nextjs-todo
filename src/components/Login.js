c371868a513e1421d61ce23223e7f806489d0c75
Victor Juarez <juarezv1990@gmail.com>
2023-04-14 08:47:00 -0400

    Removed env and adjust files

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,7 +34,7 @@ async function submitHandler() {
       <h1 className="font-extrabold text-2xl sm:text-4xl select-none">{isLoggingIn ? 'LOGIN' : 'REGISTER'}</h1>
       {error && <div className="w-full max-w-[40ch] text-center border border-rose-400 text-rose-400 py-2">{error}</div>}
       <input
-        type="text"
+        type="email"
         value={email}
         onChange={(e)=> setEmail(e.target.value)}
         className="outline-none text-slate-900 p-2 w-full max-w-[40ch] border-b-[4px] border-white border-solid focus:border-cyan-600 duration-300 rounded"
@@ -47,7 +47,7 @@ async function submitHandler() {
         className="outline-none text-slate-900 p-2 w-full max-w-[40ch] border-b-[4px] border-white border-solid focus:border-cyan-600 duration-300 rounded"
         placeholder="Password"
       />
-      <button className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 rounded duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900" onClick={submitHandler}>
+      <button className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 rounded duration-700 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-700 hover:text-slate-900" onClick={submitHandler}>
         <h2 className="z-20 relative">Submit</h2>
       </button>
       <h2 onClick={() => setIsLoggingIn(!isLoggingIn)} className='duration-300 hover:scale-110 cursor-pointer'>{!isLoggingIn ? 'Login' : 'Register'}</h2>
